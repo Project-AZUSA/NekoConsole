3df58a3aabf48d0fdb68d4a8afef3f3be4744000
UlyssesWu <wdwxy12345@gmail.com>
2018-11-16 21:45:55 +0800

    Init NekoConsole

---
+++ NekoConsole/MainWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using MahApps.Metro.Controls;
+
+namespace NekoConsole
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : MetroWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
